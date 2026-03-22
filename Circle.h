3953b5262b2ddece96655b2a829a0db97bd4ebda
build-crypto <build23705@gmail.com>
2026-03-22 19:41:47 +0700

    Create Circle.h

---
+++ Circle.h
@@ -0,0 +1,15 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+#define PI 3.14
+
+class Circle
+{
+    public:
+        void Nhap();
+        void Area();
+        void Perimeter();
+    private:
+        double X,Y,R;
+};
+
+#endif // CIRCLE_H
